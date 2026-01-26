943a5cbb57c035913cb75e2ea74fe35ef945fa91
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-26 23:13:46 +0530

    this is how i use PrintWriter Class in java

--- First_Project/src/main/java/com/Example/AddServlet.java
+++ First_Project/src/main/java/com/Example/AddServlet.java
@@ -1,15 +1,19 @@
 package com.Example;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
-	public void service(HttpServletRequest req, HttpServletResponse res) {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j= Integer.parseInt(req.getParameter("num2"));
 		int sum= i+j;
-		System.out.println("the sum is "+sum);
+		PrintWriter out = res.getWriter();
+		out.println("the sum of two number is "+ sum);
 	}
 
 }
