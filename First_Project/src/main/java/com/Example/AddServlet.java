fe3b8609ebfb7d8eccd329fa7a482b098962df19
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-01-27 21:03:37 +0530

    this is cookies

--- First_Project/src/main/java/com/Example/AddServlet.java
+++ First_Project/src/main/java/com/Example/AddServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,9 +16,12 @@ public class AddServlet extends HttpServlet{
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j= Integer.parseInt(req.getParameter("num2"));
 		int sum= i+j;
-		HttpSession session = req.getSession();
-		session.setAttribute("sum", sum);
+		Cookie cookie= new Cookie("sum", sum + "");
+		res.addCookie(cookie);
 		res.sendRedirect("sq");
+//		HttpSession session = req.getSession();
+//		session.setAttribute("sum", sum);
+//		res.sendRedirect("sq");
 //		res.sendRedirect("sq?sum="+sum); //URL Rewriting
 //		PrintWriter out = res.getWriter();
 //		out.println("the sum of two number is "+ sum);
--- First_Project/src/main/java/com/Example/SqarServlet.java
+++ First_Project/src/main/java/com/Example/SqarServlet.java
@@ -3,6 +3,7 @@ package com.Example;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -12,8 +13,15 @@ public class SqarServlet extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 //		int k = (int)req.getAttribute("sum");
 //		int k = Integer.parseInt(req.getParameter("sum"));
-		HttpSession session= req.getSession();
-		int k= (int)session.getAttribute("sum");
+//		HttpSession session= req.getSession();
+//		int k= (int)session.getAttribute("sum");
+		int k=0;
+		Cookie cookies[]= req.getCookies();
+		for(Cookie c: cookies) {
+			if(c.getName().equals("sum")) {
+				k =Integer.parseInt(c.getValue());
+			}
+		}
 		k=k*k;
 		PrintWriter out = res.getWriter();
 		out.println("Result is "+k);
